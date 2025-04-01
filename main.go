7d940c95ed4e85491cbc94e0c175ca0393799344
Bencooo <bentalha270@outlook.fr>
2025-04-01 20:12:36 +0200

    create result type

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+type Result struct {
+	Error  []string
+	Result struct {
+		Unixtime int    `json:"unixtime"`
+		Rfc1123  string `json:"rfc1123"`
+	} `json:"result"`
+}
+
 func main() {
 
 }
