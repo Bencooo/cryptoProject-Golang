6756c69638e7e9fb885c0d04df498672eb6c076b
Bencooo <bentalha270@outlook.fr>
2025-04-01 20:20:15 +0200

    create kraken call api  with getResponse function

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
 type Result struct {
 	Error  []string
 	Result struct {
@@ -8,6 +15,23 @@ type Result struct {
 	} `json:"result"`
 }
 
-func main() {
+func getResponse() string {
+	resp, err := http.Get("https://api.kraken.com/0/public/Time")
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if body != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Response Body :", body)
+	return string(body)
+}
+
+func main() {
+	getResponse()
 }
