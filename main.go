d0ef2c9c7c8adca90ea00cad0960dabf5a108f1d
Bencooo <bentalha270@outlook.fr>
2025-04-01 20:32:49 +0200

    create parseResponse

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -17,21 +18,34 @@ type Result struct {
 
 func getResponse() string {
 	resp, err := http.Get("https://api.kraken.com/0/public/Time")
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
-	if body != nil {
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Response Body :", body)
+	fmt.Println("Response Body :", string(body))
 	return string(body)
 }
 
+func parseResponse(body string) Result {
+	var value Result
+	err := json.Unmarshal([]byte(body), &value)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return value
+}
+
 func main() {
-	getResponse()
+	response := getResponse()
+	result := parseResponse(response)
+
+	fmt.Println(result.Result)
+	fmt.Println(result.Result.Unixtime)
+	fmt.Println(result.Result.Rfc1123)
 }
